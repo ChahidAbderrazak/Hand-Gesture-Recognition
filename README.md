9826e17bd15c9652d6f94c82cc454dda2b01d15d
ChahidAbderrazak <abderrazak.chahid@gmail.com>
2025-05-30 06:09:27 -0400

    restructure the code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Hand-Gesture-Recgnition
+# Hand Gesture Recognition
+A Quantization-based position Weight Matrix (QuPWM) feature extraction method for mult-iclass classification is proposed to improve the interpretation of biomedical signals. This method is validated on surface Electromyogram (sEMG) signals recognition for eight different hand gestures.
+
+## Running
+2- Use the main file: main.m
+
+# Acknowledgement
+The work is the result of research supported by King Abdullah University of Science and Technology [(KAUST)](https://www.kaust.edu.sa/en/) in collaboration with University of Technology, Sydney [(UTS)](https://www.uts.edu.au/). The authors would like to thank the students who participated in different phases of the project development and testing. For more details about the research team, please take a look at the published [paper](https://ieeexplore.ieee.org/abstract/document/9176097).
--- main.m
+++ main.m
@@ -9,14 +9,14 @@
 %
 %% ###########################################################################
 clc; clear all;  close all ;format shortG;  addpath ./Functions ;Include_function ;%log_html_file
-global y h filename  root_folder 
+global y h filename  root_folder
 
 % Data sources
 root_OS='/Volumes/eman/chahida/';sep='/';% 'R:/chahida/';sep='\';%
 
 data_Source=strcat(root_OS,'Projects-Dataset/sEMG-Rami/Extracted_gesture_data/');
-path_Classification='/QuPWM_ALL_feature/';             % Destination 
-%project_folder=pwd;project_folder = strsplit(project_folder,'\'); 
+path_Classification='/QuPWM_ALL_feature/';             % Destination
+%project_folder=pwd;project_folder = strsplit(project_folder,'\');
 
 project_folder=path_Classification
 path_Classification=char(strcat(root_OS,'Projects-Results/',char(project_folder(end)),path_Classification));
@@ -30,21 +30,21 @@ for file_k=2%:size(List_Data_files,1)
     file_k
     filename=List_Data_files(file_k).name;
     data_Source=List_Data_files(file_k).folder;
-    cname=strcat(data_Source,sep, filename);   
-    load(cname); 
+    cname=strcat(data_Source,sep, filename);
+    load(cname);
 
     %% Apply this script on the current data file
-    Hand_Gesture_Classification_using_kmer_mPWM     
+    Hand_Gesture_Classification_using_kmer_mPWM
 
-    %% Save partially Obtained results 
+    %% Save partially Obtained results
     Comp_results_aLL=[Comp_results_aLL;Comp_results_Table];
 
     save(strcat(path_Classification,'MC_',noisy_file,CV_type,'_',type_clf,'_On',string(datetime('now','Format','yyyy-MM-dd''T''HHmmss')),'.mat'),...
-                 'perform_output','Comp_results_Table','noisy_file','data_Source','cname','perform_output')                                                                                                                    
+                 'perform_output','Comp_results_Table','noisy_file','data_Source','cname','perform_output')
 
 end
 
-%% Display resutls 
+%% Display resutls
 Comp_results_aLL
 
 %% Save Obtained results on all the dataset
@@ -53,10 +53,8 @@ Comp_results_aLL
 writetable(Comp_results_aLL,strcat(path_Classification,'AllData_',num2str(size(List_Data_files,1)),pwm_param,noisy_file,suff,'_On',string(datetime('now','Format','yyyy-MM-dd''T''HHmmss')),'.xlsx'))
 
 save(strcat(path_Classification,'AllData_',num2str(size(List_Data_files,1)),pwm_param,noisy_file,suff,'_On',string(datetime('now','Format','yyyy-MM-dd''T''HHmmss')),'.mat'),...
-           'Comp_results_aLL','List_Data_files','data_Source','perform_output')                                                                                                                    
+           'Comp_results_aLL','List_Data_files','data_Source','perform_output')
 
 %winopen(path_Classification);
 
 fprintf('\n################  The End ################\n\n')
-                                   
-                                   
